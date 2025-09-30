80ff8c83f1b091f0d9654aef8d2b1c189a87ecfc
Dayana Keo <keo.dayana@hotmail.fr>
2025-09-30 14:37:30 +0200

    affichage de la vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,10 @@
 <template>
     <h1>Chat Room</h1>
     <ChatRoom />
+    <slot />
+    <PassphraseModal />
 </template>
+
+<script setup>
+import PassphraseModal from '~/components/PassphraseModal.vue'
+</script>
